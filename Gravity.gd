5e15738e8327b99f8c077ff0d6b97ea17f387672
MFFitzGerald <mff1075@gmail.com>
2019-05-25 18:27:00 -0400

    Gravity actually works now

--- Gravity.gd
+++ Gravity.gd
@@ -1,19 +1,23 @@
 extends KinematicBody2D
 
 # Member variables
-const GRAVITY = 500.0 # pixels/second/second
+const GRAVITY = 10.0 # pixels/second/second
 
 # Angle in degrees towards either side that the player can consider "floor"
-const FLOOR_ANGLE_TOLERANCE = 40
+const FLOOR_ANGLE_TOLERANCE = 90
 const WALK_FORCE = 600
 const WALK_MIN_SPEED = 10
 const WALK_MAX_SPEED = 200
 const STOP_FORCE = 1300
 const JUMP_SPEED = 200
-const JUMP_MAX_AIRBORNE_TIME = 0.2
+const JUMP_MAX_AIRBORNE_TIME = 0.8
 
 const SLIDE_STOP_VELOCITY = 1.0 # one pixel/second
 const SLIDE_STOP_MIN_TRAVEL = 1.0 # one psixel
+
+const SPEED = 200
+const JUMP_HEIGHT = -550
+const UP = Vector2(0, -1)
 
 var velocity = Vector2()
 var on_air_time = 100
@@ -21,10 +25,12 @@ var jumping = false
 
 var prev_jump_pressed = false
 
+var motion = Vector2()
 
 func _physics_process(delta):
 	# Create forces
 	var force = Vector2(0, GRAVITY)
+	motion.y += GRAVITY
 	
 	var walk_left = Input.is_action_pressed("move_left")
 	var walk_right = Input.is_action_pressed("move_right")
@@ -71,4 +77,20 @@ func _physics_process(delta):
 	
 	on_air_time += delta
 	prev_jump_pressed = jump
+	
+	
+	#movement code
+	
+
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+	else:
+		motion.x = 0
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+	motion = move_and_slide(motion, UP)
+	pass
 	
